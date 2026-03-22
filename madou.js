9cd248f14794f9ab86b4cdd5ef5dac8851b9f68a
zhanghoa <42800990+zhanghoa@users.noreply.github.com>
2026-03-22 20:21:50 +0800

    Update madou.js

--- madou.js
+++ madou.js
@@ -1,5 +1,5 @@
 var rule = {
-    title: '麻豆官方-终极修复',
+    title: '麻豆官方-高速直连版',
     host: 'https://madou.com',
     url: '/topic/0/fyclass/fypage/',
     searchUrl: '/searchvideo/**/fypage/',
@@ -9,47 +9,15 @@ var rule = {
     headers: {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
         'Referer': 'https://madou.com/',
-        'Cookie': 'isAgeModalShownKey=true; is_mobile=1'
+        'Cookie': 'isAgeModalShownKey=true'
     },
     class_name: '精选好片&MSD系列&淫欲中国&传媒片商&国产视频&日本AV&欧美中字',
     class_url: '13678&13679&13682&30521&30526&30522&13726',
     play_parse: true,
-
-    一级: "js: var items = []; var html = request(input); var m = html.match(/var dataJson = '(.*?)';/); if (m) { var raw = m[1].replace(/\\\\u0022/g, '\"').replace(/\\\\u002f/g, '/'); try { var data = JSON.parse(raw).data; var list = data.videoInfos || data.videoInfosList || []; list.forEach(function(it) { if (it.id > 0) { items.push({ title: it.title, img: it.coverImg, desc: it.creatorName || '麻豆', url: '/archives/' + it.id + '/' }); } }); } catch(e) {} } setResult(items);",
+    一级: "js: var items=[]; var html=request(input); var m=html.match(/dataJson\\s*=\\s*'(.*?)'/); if(m){ var raw=m[1].replace(/\\\\u0022/g,'\"').replace(/\\\\u002f/g,'/').replace(/\\\\u0026/g,'&'); try { var res=JSON.parse(raw); var list=res.data.videoInfos||res.data.videoInfosList||[]; list.forEach(function(it){ if(it.id>0){ items.push({title:it.title, img:it.coverImg, desc:it.creatorName||'麻豆', url:'/archives/'+it.id+'/'}); } }); } catch(e){} } setResult(items);",
     
-    // 【核心改进】二级详情页采用多重匹配，防崩逻辑
-    二级: "js: var html = request(input); var VOD = { vod_name: '解析失败', vod_remarks: '请刷新' }; \
-        try { \
-            var m_json = html.match(/var dataJson = '(.*?)';/); \
-            if (m_json) { \
-                var raw = m_json[1].replace(/\\\\u0022/g, '\"').replace(/\\\\u002f/g, '/').replace(/\\\\u0026/g, '&'); \
-                var res = JSON.parse(raw); \
-                var info = res.data.videoInfo || (res.data.videoInfos ? res.data.videoInfos[0] : res.data); \
-                VOD.vod_name = info.title || '无标题'; \
-                VOD.vod_pic = info.coverImg || ''; \
-                VOD.vod_content = info.desc || info.intro || '暂无剧情简介'; \
-                VOD.vod_remarks = info.creatorName || '官方源'; \
-                \
-                /* 提取线路地址 */ \
-                var playPath = ''; \
-                if (info.video && info.video.url) { \
-                    playPath = info.video.url; \
-                } else { \
-                    var m_path = html.match(/const path = \"(.*?)\";/); \
-                    if (m_path) playPath = m_path[1].replace(/\\\\u0026/g, '&'); \
-                } \
-                \
-                if (playPath) { \
-                    VOD.vod_play_from = '核心线路(快)$$$官方备用'; \
-                    var urlA = 'https://lmdi048.com/h5/m3u8/' + playPath; \
-                    var urlB = 'https://madou.com/h5/m3u8/' + playPath; \
-                    VOD.vod_play_url = '播放$' + urlA + '$$$播放$' + urlB; \
-                } \
-            } \
-        } catch(e) { \
-            VOD.vod_content = '解析出错:' + e.message; \
-        } \
-        setResult(VOD);",
-
-    lazy: "js: input = { jx: 0, url: input.split('$')[1], parse: 0, header: JSON.stringify(rule.headers) };"
+    // 【核心修正】直接对接 Cloudfront 高速节点按钮
+    二级: "js: var html=request(input); var VOD={vod_name:'视频详情',vod_remarks:'高速直连'}; try { var m_json=html.match(/dataJson\\s*=\\s*'(.*?)'/); if(m_json){ var raw=m_json[1].replace(/\\\\u0022/g,'\"').replace(/\\\\u002f/g,'/').replace(/\\\\u0026/g,'&'); var res=JSON.parse(raw); var info=res.data.videoInfo||(res.data.videoInfos?res.data.videoInfos[0]:res.data); VOD.vod_name=info.title; VOD.vod_pic=info.coverImg; VOD.vod_content=info.desc||'暂无简介'; var playPath=''; if(info.video&&info.video.url){ playPath=info.video.url; } else { var m_path=html.match(/path\\s*=\\s*\"(.*?)\"/); if(m_path) playPath=m_path[1].replace(/\\\\u0026/g,'&'); } if(playPath){ VOD.vod_play_from='Cloudfront高速线$$$官方主线'; var urlA='https://d2r1iw2cxonh4q.cloudfront.net/h5/m3u8/'+playPath; var urlB='https://madou.com/h5/m3u8/'+playPath; VOD.vod_play_url='播放$'+urlA+'$$$播放$'+urlB; } } } catch(e){ VOD.vod_content='解析报错:'+e.message; } setResult(VOD);",
+    
+    lazy: "js: input = {jx:0, url:input.split('$')[1], parse:0, header:JSON.stringify(rule.headers)};"
 };
